56f137ebf80d7c37cca6fa2a1b382c367bba09dd
marwai <mtse@spartaglobal.com>
2020-07-07 00:20:24 +0100

    status

--- devops_student.py
+++ devops_student.py
@@ -8,18 +8,18 @@ class Devops(Student): # The class inherits all the characteristics from student
         super().__init__(name, age, course)
         self.skills = skills # unique to devops only
 
-
+    # defines methods in child class which are define in the parent class
     def new_skill(self): # method is unique to Person
         self.details()
         print("You have learned " + self.skills, "at Sparta global\n")
+    def intro(self):
+        print(self.name, "is a spartan learning" ,self.skills)
 
-# instance of method has been named
-Marcus = Devops("Marcus", "23" , "Aerospace Engineering", "SQL\n") #\n creates a new line
+# bob has become a spartan with additional skills
+a =  Devops("bob", "25", "Computer Science", "SQL, Python and AWS\n")
+# introduction of bob before joining sparta
+b = Student("bob", "25", "Computer Science\n")
 
-#Recalls the instance of the class
-Marcus.new_skill()
-
-
-bob =  Devops("bob", "25", "Computer Science", "SQL, Python and AWS\n")
-bob.new_skill()
-
+# intro() has two different functions
+b.intro() # acquires method from Student class
+a.intro() # Acquires characteristics from Devops Class
--- student_data.py
+++ student_data.py
@@ -3,22 +3,20 @@ class Student:
     # method (behaviour) initialises the variables
     def __init__(self, name, age, course): # (name, age, course attributes are defined)
         self.name = name
-        self.__age = age
+        self.age = age
         self.course = course
 
     # Details of the student
     def details(self):
         # basic print functions
         print("Name: " + self.name)
-        print("Age: " + self.__age)
+        print("Age: " + self.age)
         print("University course: " + self.course)
 
-# Adds the strings together
-a = "hel"
-b = "lo"
-print(a+b) # concatenation of the string
+    def age(self):
+        print(self.name, "is {} years old".format(self.age))
 
-# Adds the numbers together
-a = 5
-b = 6
-print(a+6) # built in python example of polymorphism
+    def intro(self):
+        print(self.name ,"is a student")
+
+
